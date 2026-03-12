16821182e079afb05b7d08f0191abcea78ed6b87
Augusto Caesar <aoghast@gmail.com>
2026-03-12 17:08:34 -0300

    finished integrating api with frontend and printed a simple json message

--- apps/angular-nest-fullstack-app/src/app/app.ts
+++ apps/angular-nest-fullstack-app/src/app/app.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
+import { Message } from '@angular-nest-fullstack-app/interfaces';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [NxWelcome, RouterModule, AsyncPipe, JsonPipe],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  protected title = 'angular-nest-fullstack-app';
+  hello$ = inject(HttpClient).get<Message>('/api');
 }
--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-
+import { Message } from '@angular-nest-fullstack-app/interfaces';
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
+  getData(): Message {
     return { message: 'Hello API' };
   }
 }
